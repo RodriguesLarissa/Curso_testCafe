2499ed52fcf55f9a14f941aa3cc12848c01331bd
Larissa <rodrigues_larissa@outlook.com>
2021-07-14 14:53:50 -0300

    Data driven tests

--- test/RegistrationTest.js
+++ test/RegistrationTest.js
@@ -5,10 +5,10 @@ import registerpage from '../pages/RegisterPage';
 import loginpage from '../pages/LoginPage';
 import myaccountpage from '../pages/MyAccountPage';
 
+const dataSet = require('../data/data.json');
 const URL = "https://demo.nopcommerce.com/";
 const getURL = ClientFunction(() => window.location.href);
 var randomNumber = Math.floor(Math.random() * 10000);
-var userEmail = 'larissa'+randomNumber+'@test.com';
 
 fixture("Registration Fixture")
     .page(URL);
@@ -20,36 +20,38 @@ test('Assert Home Page Test', async t =>{
         .expect(homepage.subtitleHeader.exists).ok();
 });
 
-test('User Registration and Login Test', async t =>{
-    //Register
-    await t
-        .click(homepage.registerLink)
-        .expect(getURL()).contains('register')
-        .click(registerpage.genderOption)
-        .typeText(registerpage.firstName,'Larissa')
-        .typeText(registerpage.lastName,'Rodrigues');
+dataSet.forEach(data =>{
+    test('User Registration and Login Test', async t =>{
+        //Register
+        await t
+            .click(homepage.registerLink)
+            .expect(getURL()).contains('register')
+            .click(registerpage.genderOption)
+            .typeText(registerpage.firstName, data.firstname)
+            .typeText(registerpage.lastName, data.lastname);
 
-    await registerpage.selectDay('13');
-    await registerpage.selectMonth('September');
-    await registerpage.selectYear('1999');
+        await registerpage.selectDay(data.birthday);
+        await registerpage.selectMonth(data.birthmonth);
+        await registerpage.selectYear(data.birthyear);
 
-    await t
-        .typeText(registerpage.email, userEmail)
-        .typeText(registerpage.password, '123456')
-        .typeText(registerpage.confirmPassword, '123456')
-        .click(registerpage.registerButton)
-        .expect(registerpage.successfullMessage.exists).ok()
+        await t
+            .typeText(registerpage.email, data.email+randomNumber+'@test.com')
+            .typeText(registerpage.password, data.password)
+            .typeText(registerpage.confirmPassword, data.password)
+            .click(registerpage.registerButton)
+            .expect(registerpage.successfullMessage.exists).ok()
 
-    //Login
-        .click(homepage.logOutLink)
-        .click(homepage.loginLink)
-        .expect(loginpage.accountHeader.exists).ok()
-        .typeText(loginpage.emailInput, userEmail)
-        .typeText(loginpage.passwordInput, '123456')
-        .click(loginpage.submitButton)
-    
-    //My Account Page
-        .expect(myaccountpage.ordersLink.exists).ok()
-        .click(myaccountpage.ordersLink)
-        .expect(myaccountpage.noOrdersLabel.exists).ok();
+        //Login
+            .click(homepage.logOutLink)
+            .click(homepage.loginLink)
+            .expect(loginpage.accountHeader.exists).ok()
+            .typeText(loginpage.emailInput, data.email+randomNumber+'@test.com')
+            .typeText(loginpage.passwordInput, data.password)
+            .click(loginpage.submitButton)
+        
+        //My Account Page
+            .expect(myaccountpage.ordersLink.exists).ok()
+            .click(myaccountpage.ordersLink)
+            .expect(myaccountpage.noOrdersLabel.exists).ok();
+    });
 });
